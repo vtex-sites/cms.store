147dcc9d43f764aacfbdd93194c03000744fe8be
Juliana Damurie <juliana.damurie@vtex.com.br>
2021-12-08 18:54:16 -0300

    remove all

--- src/components/ui/Checkbox/Checkbox.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Checkbox as UICheckbox } from '@faststore/ui'
-import type { CheckboxProps } from '@faststore/ui'
-
-function Checkbox(props: CheckboxProps) {
-  return <UICheckbox {...props} />
-}
-
-export default Checkbox
